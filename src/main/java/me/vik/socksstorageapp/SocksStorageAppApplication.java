5bca8e6c063374f890297bce523debe7d91dc454
VitaliyK <Vik163tlt@ya.ru>
2023-01-29 17:11:39 +0300

    New

---
+++ src/main/java/me/vik/socksstorageapp/SocksStorageAppApplication.java
@@ -0,0 +1,13 @@
+package me.vik.socksstorageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStorageAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStorageAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/vik/socksstorageapp/SocksStorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vik.socksstorageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStorageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
